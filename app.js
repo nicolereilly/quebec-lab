683354badb156f7b4604fcb503ba86c758252f79
Nicole Reilly <nreilly@una.edu>
2023-03-06 19:50:35 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
-require('dotenv').config()
+require("dotenv").config()
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000;  
 const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
 const { ObjectId } = require('mongodb')
+const PORT = process.env.PORT || 3000;
+const herokuVar = process.env.HEROKU_NAME || "local Barry"
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
-
-// const uri = "mongodb+srv://kwilliams31:9MVfWqRjRU3DvosT@cluster0.fmwwnxu.mongodb.net/?retryWrites=true&w=majority";
-
-//connecting to his db
-// const uri = "mongodb+srv://barry:nb3amjtQWhSN6ibH@cluster0.taug6.mongodb.net/?retryWrites=true&w=majority"; 
-
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const MONGO_URI = "mongodb+srv://nreilly:vY4A1eYR0sVUpISS@cluster0.6kyiorf.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
 
-app.set('view engine', 'ejs');
 
 async function cxnDB(){
 
   try{
     client.connect; 
     const collection = client.db("papa-lab").collection("dev-profiles");
-
-    // const collection = client.db("papa").collection("dev-profiles");
     const result = await collection.find().toArray();
-    //const result = await collection.findOne(); 
     console.log("cxnDB result: ", result);
     return result; 
   }
@@ -40,7 +32,6 @@ async function cxnDB(){
 
 
 app.get('/', async (req, res) => {
-  //res.send('his this is she! <br/> <a href="mongo">mongo</a>');
 
   let result = await cxnDB().catch(console.error); 
 
@@ -51,7 +42,6 @@ app.get('/', async (req, res) => {
 
 app.get('/mongo', async (req, res) => {
 
-  // res.send("check your node console, bro");
 
   let result = await cxnDB().catch(console.error); 
 
@@ -63,11 +53,9 @@ app.get('/mongo', async (req, res) => {
 
 app.get('/update', async (req, res) => {
 
-    // want to get data from the form
     console.log("in get to slash update:", req.query.ejsFormName); 
     myName = req.query.ejsFormName; 
 
-    // update into the db
     client.connect; 
     const collection = client.db("papa-lab").collection("dev-profiles");
 
@@ -80,7 +68,7 @@ app.get('/update', async (req, res) => {
 app.post('/addName', async (req, res) => {
 
   try {
-    // console.log("req.body: ", req.body) 
+
     client.connect; 
     const collection = client.db("papa-lab").collection("dev-profiles");
     await collection.insertOne(req.body);
@@ -91,7 +79,6 @@ app.post('/addName', async (req, res) => {
     console.log(error)
   }
   finally{
-   // client.close()
   }
 
 })
@@ -116,7 +103,6 @@ app.post('/deleteName/:id', async (req, res) => {
     .catch(error => console.error(error))
   }
   finally{
-    //client.close()
   }
 
 })
